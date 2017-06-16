49f67b7dabf292b134d8d7ea1428d4967c1b14fc
Ilya-R <megabonus777@mail.ru>
2017-06-17 01:28:43 +1000

    полностью готово к деплою на heroku

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,10 @@ Rails.application.routes.draw do
 
   # ресурс вопросов (кроме экшенов :show, :new, :index)
   resources :questions, except: [:show, :new, :index]
-  
+
+
+
+  get 'sign_up' => 'users#new'
+  get 'log_out' => 'sessions#destroy'
+  get 'log_in' => 'sessions#new'
 end
